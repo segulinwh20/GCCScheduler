68c513cd987134556cbda9742c99cffb63bca555
Jsouthern777 <southernjj21@gcc.edu>
2024-03-18 19:28:16 -0400

    worked on course file IO

--- src/main/java/com/classes/Search.java
+++ src/main/java/com/classes/Search.java
@@ -1,17 +1,13 @@
 package com.classes;
 
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
 public class Search {
-
-    //List of courses to be input from file
     private List<Course> courses;
-
-    //List of filters that I will modify
     private List<String> filters;
 
     public List<Course> search() {
@@ -31,33 +27,106 @@ public class Search {
     }
 
     public void clearFilters() {
-        filters.clear();
+
     }
 
-    public List<Course> readCoursesFromFile(String filepath) throws IOException{
-        String filepathString = Files.readString(Path.of("searchTest.txt"));
-        Scanner scn = new Scanner(filepathString);
-        while (scn.hasNextLine()){
-            String[] tokens = scn.nextLine().split(",");
-            String semester = tokens[0];
-            String department = tokens[1];
-            int id = Integer.parseInt(tokens[2]);
-            char sectionLetter = tokens[3].charAt(0);
-            String courseCode = tokens[1] + " " + tokens[2] + " " + tokens[3];
-            String title = tokens[4];
-            int credits = Integer.parseInt(tokens[5]);
-            int seats = (Integer.parseInt(tokens[6])-Integer.parseInt(tokens[7]));
-            String M = tokens[8];
-            String T = tokens[9];
-            String W = tokens[10];
-            String R = tokens[11];
-            String F = tokens[12];
-            String start = tokens[13];
-            String end = tokens[14];
-            String professor =  tokens[15] + ", "  + tokens[16];
-            String description = tokens[18];
-            //TODO: Create the timeslot here (check that a day isn't empty, parse time)
+    public static List<Course> readCoursesFromFile(String filepath) {
+        List<Course> c = new ArrayList<Course>();
+        try(Scanner inFile = new Scanner(new File(filepath))){
+            String line = inFile.nextLine(); // removes header line
+            String semester;
+
+            while(inFile.hasNextLine()){
+                line = inFile.nextLine();
+                String[] fields = line.split(",");
+
+                if(fields.length != 20){ // fixed errors due to empty values at end of csv
+                    String[] hold = new String[20];
+                    for (int i = 0; i < fields.length; i++) {
+                        hold[i] = fields[i];
+                    }
+                    fields = new String[20];
+                    for (int i = 0; i < 20; i++) {
+                        fields[i] = hold[i];
+
+                    }
+                }
+
+                if(Integer.parseInt(fields[1]) == 10) {
+                    semester = "fall";
+                } else {
+                    semester = "spring";
+                }
+
+                Professor prof = new Professor();
+
+                prof.setFirst(fields[18]);
+                prof.setLast(fields[17]);
+                prof.setDepartment(fields[2]);
+
+                String[] beginTimeData = fields[14].split(":");
+                String[] endTimeData = fields[15].split(":");
+
+                if(beginTimeData[0].isEmpty()){ // fixed error were time slots were empty
+                    beginTimeData = new String[3];
+                    beginTimeData[0] = "0";
+                    beginTimeData[1] = "0";
+                    beginTimeData[2] = "NONE";
+                }
+
+                int startHour = Integer.parseInt(beginTimeData[0]);
+
+                if(beginTimeData[2].charAt(3) == 'P'){
+                    startHour += 12;
+                }
+
+                if(endTimeData[0].isEmpty()) {
+                    endTimeData = new String[3];
+                    endTimeData[0] = "0";
+                    endTimeData[1] = "0";
+                    endTimeData[2] = "NONE";
+                }
+
+                int endHour = Integer.parseInt(endTimeData[0]);
+
+                if(endTimeData[2].charAt(3) == 'P'){
+                    endTimeData[0] += 12;
+                }
+
+
+                // note, days of the week are field[9] - field[13]
+                String daysOfWeek = fields[9] + fields[10] + fields[11] + fields[12] + fields[13];
+                System.out.println(daysOfWeek);
+                //TODO: Fix TimeSlot to allow for multiple days of the week
+                TimeSlot timeslot = new TimeSlot('M', startHour,
+                        Integer.parseInt(beginTimeData[1]),endHour,
+                        Integer.parseInt(endTimeData[1]));
+
+                List<TimeSlot> time = new ArrayList<TimeSlot>();
+                time.add(timeslot);
+
+
+                char sectionLetter; // fixed error when the section letter is empty
+                if(fields[4].isEmpty()){
+                    sectionLetter = ' ';
+                } else {
+                    sectionLetter = fields[4].charAt(0);
+                }
+
+                // made course constructor with all information in CSV that lined up with variables already present
+                Course course = new Course(Integer.parseInt(fields[3]), fields[5],
+                        Integer.parseInt(fields[7]), fields[2],fields[19], fields[0],
+                        semester, prof, time, sectionLetter, Integer.parseInt(fields[7]));
+
+
+                c.add(course);
+
+
+            }
+            return c;
         }
-        return null;
+        catch (FileNotFoundException e){
+            throw new RuntimeException("NO FILE");
+        }
     }
 }
---
+++ src/test/java/com/classes/FileIOTest.java
@@ -0,0 +1,16 @@
+package com.classes;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+class FileIOTest {
+
+    @Test
+    void readCoursesFromFile() {
+
+        List<Course> c = Search.readCoursesFromFile("data/2020-2021.csv");
+        System.out.println(c.get(0).getId());
+
+    }
+}
